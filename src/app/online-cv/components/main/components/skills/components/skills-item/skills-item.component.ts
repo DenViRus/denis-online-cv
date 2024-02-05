c88e8ff71246077349c1aabb72575505c16d2325
Denis Yefremenko <denvirus83@mail.ru>
2024-02-05 10:34:17 +0100

    feat: add animation for section projects, add animation for section skills

--- src/app/online-cv/components/main/components/skills/components/skills-item/skills-item.component.ts
+++ src/app/online-cv/components/main/components/skills/components/skills-item/skills-item.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input  } from '@angular/core';
 
 import { ISkillsItem } from '../../models/skills-item.model';
 
@@ -12,4 +12,20 @@ import { ISkillsItem } from '../../models/skills-item.model';
 })
 export class SkillsItemComponent {
   @Input() public itemData!: ISkillsItem;
+
+  @HostBinding('style.--level-width') get levelWidth() {
+    return `${this.itemData.level}%`;
+  }
+
+  @HostBinding('style.--level-width-5') get levelWidth5() {
+    return `${this.itemData.level + 5}%`;
+  }
+
+  @HostBinding('style.--level-width-10') get levelWidth10() {
+    return `${this.itemData.level + 10}%`;
+  }
+
+  @HostBinding('style.--level-width-15') get levelWidth15() {
+    return `${this.itemData.level + 15}%`;
+  }
 }
